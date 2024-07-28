faba3f1add1f39d6e9d0d6afe2976cffda970b5f
pablo2389 <160980965+pablo2389@users.noreply.github.com>
2024-07-28 08:59:19 -0300

    Add files via upload

---
+++ ejercicios_profundizacion/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import './App.css'; // Si tienes estilos globales
+
+import NoteApp from './NoteApp';
+
+function App() {
+  return (
+    <div className="App">
+      <NoteApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ ejercicios_profundizacion/NoteApp.js
@@ -0,0 +1,43 @@
+// src/NoteApp.js
+import React, { useState } from 'react';
+
+function NoteApp() {
+  const [notes, setNotes] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleAddNote = () => {
+    if (inputValue.trim() !== '') {
+      setNotes([...notes, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  const handleDeleteNote = (index) => {
+    const updatedNotes = [...notes];
+    updatedNotes.splice(index, 1);
+    setNotes(updatedNotes);
+  };
+
+
+
+return (
+      <div>
+      <h1>Notas</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={handleAddNote}>Agregar Nota</button>
+      <ul>
+        {notes.map((note, index) => (
+          <li key={index} className="note">
+            {note}
+            <button onClick={() => handleDeleteNote(index)}>Eliminar</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default NoteApp;
